7146cc2514fb9cf8d12b5b60535aa752e64e42dd
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-10 12:00:48 +0900

    add button component

---
+++ lib/ui_component/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shogi_game/constraints/app_color.dart';
+
+class Button extends StatelessWidget {
+  final String buttonText;
+  final void Function() onTap;
+  const Button({Key? key, required this.buttonText, required this.onTap})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width - 48,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: AppColor.buttonColor,
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: AppColor.textColorWhite,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
