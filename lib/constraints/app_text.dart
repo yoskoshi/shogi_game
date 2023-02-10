11a3a9d1dafa324a55b56b762a837ce9dfe8f12a
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-10 16:08:14 +0900

    add constraints text

--- lib/constraints/app_text.dart
+++ lib/constraints/app_text.dart
@@ -8,6 +8,12 @@ class AppText {
   static const String gameSettings = "game settings";
   static const String startGame = "ゲーム開始！";
   static const String firstMove = "先手";
+  static const String secondMove = "後手";
   static const String ifFirstMoveOffBecomeSecondMove = "先手をoffにすると後手になります";
   static const String name = "名前";
+  static const String inputName = "名前を入力する";
+  static const String numberOfPeople = "人数";
+  static const String tenPeople = "10人";
+  static const String allottedTime = "持ち時間";
+  static const String tenMinutes = "10分";
 }
