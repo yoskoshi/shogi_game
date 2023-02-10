d8c8ca5603c008482b4dcb089abb6fcb6e8f221b
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-10 13:19:12 +0900

    change app_color, app_text constraints

--- lib/constraints/app_color.dart
+++ lib/constraints/app_color.dart
@@ -2,6 +2,8 @@ import 'package:flutter/animation.dart';
 
 class AppColor {
   static const Color luminosityColor = Color(0x33000000);
-  static const Color textColorWhite = Color(0xFFFFFFFF);
+  static const Color white = Color(0xFFFFFFFF);
   static const Color buttonColor = Color(0xFFB22222);
+  static const Color textColorBlack = Color(0xFF000000);
+  static const Color textColorGray = Color(0xFF808080);
 }
--- lib/constraints/app_text.dart
+++ lib/constraints/app_text.dart
@@ -5,4 +5,9 @@ class AppText {
   static const String onePlayerBattle = "1人で対戦する";
   static const String twoPlayersBattle = "2人で対戦する";
   static const String manyPlayersBattle = "大人数で対戦する";
+  static const String gameSettings = "game settings";
+  static const String startGame = "ゲーム開始！";
+  static const String firstMove = "先手";
+  static const String ifFirstMoveOffBecomeSecondMove = "先手をoffにすると後手になります";
+  static const String name = "名前";
 }
