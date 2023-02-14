5742bfeb20dcdbc76b8d0f3710d3dc6a379a9d4f
yoskoshi <8423koshi@ezweb.ne.jp>
2023-02-14 21:02:13 +0900

    add text provider

--- lib/ui_component/input_name.dart
+++ lib/ui_component/input_name.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shogi_game/constraints/app_color.dart';
 import 'package:shogi_game/constraints/app_text.dart';
+import 'package:shogi_game/providers/one_player_game_setting_notifier.dart';
 
-class InputName extends StatelessWidget {
+class InputName extends ConsumerWidget {
   final TextEditingController controller;
   const InputName({Key? key, required this.controller}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
         const Expanded(
@@ -23,6 +26,11 @@ class InputName extends StatelessWidget {
           width: 170,
           child: TextField(
             controller: controller,
+            onSubmitted: (value) {
+              final onePlayerGameSettingNotifier =
+                  ref.read(onePlayerGameSettingProvider.notifier);
+              onePlayerGameSettingNotifier.updateName(controller.text);
+            },
           ),
         ),
       ],
